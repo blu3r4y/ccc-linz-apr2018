d9ffab86d52d3a2111c0587505e25dc6aa21943a
Mario Kahlhofer <mario.kahlhofer@gmail.com>
2018-04-27 16:12:53 +0200

    level 2

--- CCC_Linz18Spring/Program.cs
+++ CCC_Linz18Spring/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Globalization;
 using System.IO;
 using System.Linq;
+using MathNet.Numerics.LinearAlgebra;
+using MathNet.Spatial.Euclidean;
 using Newtonsoft.Json;
 using Whiteparse;
 
@@ -14,43 +16,55 @@ namespace CCC_Linz18Spring
         {
             for (var i = 0; i <= 4; i++)
             {
-                var obj = JsonConvert.DeserializeObject<RootObject>(
-                    File.ReadAllText($"C:\\data\\Dropbox\\Projekte\\Code\\CCC_Linz18Spring\\data\\level1\\lvl1-{i}.json"));
+                var obj = JsonConvert.DeserializeObject<Input>(
+                    File.ReadAllText($"C:\\data\\Dropbox\\Projekte\\Code\\CCC_Linz18Spring\\data\\level2\\lvl2-{i}.json"));
                 string solved = SolveLevel(obj);
                 Console.WriteLine(solved);
-                File.WriteAllText($"C:\\data\\Dropbox\\Projekte\\Code\\CCC_Linz18Spring\\data\\level1\\solution-{i}.txt", solved);
+                File.WriteAllText($"C:\\data\\Dropbox\\Projekte\\Code\\CCC_Linz18Spring\\data\\level2\\solution-{i}.txt", solved);
             }
         }
 
-        private static string SolveLevel(RootObject rootObject)
+        private static string SolveLevel(Input input)
         {
-            var sortedList = new List<int>();
-            
-            foreach (Image img in rootObject.images)
-            {
-                bool foundAsteroid = img.rows.SelectMany(e => e).Count(e => e > 0) > 0;
-                if (foundAsteroid) sortedList.Add(img.timestamp);
-            }
-            
-            sortedList.Sort();
+            var observations = new List<Observation>();
 
-            return string.Join("\n", sortedList);
+            foreach (Image img in input.images)
+            {
+                if (img.HasAsteroid())
+                {
+                    // search for existing observations
+                    var match = false;
+                    foreach (Observation obsv in observations)
+                    {
+                        if (Image.EqualAsteroid(obsv.asteroid, img.GetAsteroid()))
+                        {
+                            obsv.count++;
+                            if (img.timestamp > obsv.lastSeen)
+                                obsv.lastSeen = img.timestamp;
+
+                            match = true;
+                        }
+                    }
+
+                    if (!match)
+                    {
+                        // append new
+                        observations.Add(
+                            new Observation {firstSeen = img.timestamp, lastSeen = img.timestamp, asteroid = img.GetAsteroid()});
+                    }
+                }
+            }
+
+            return string.Join("\n", observations.OrderBy(e => e.firstSeen).Select(e => $"{e.firstSeen} {e.lastSeen} {e.count}"));
         }
     }
 
-    public class Image
+    public class Observation
     {
-        public int timestamp { get; set; }
-        public int rowcount { get; set; }
-        public int colcount { get; set; }
-        public List<List<int>> rows { get; set; }
-    }
+        public int? firstSeen;
+        public int? lastSeen;
+        public int count = 1;
 
-    public class RootObject
-    {
-        public int start { get; set; }
-        public int end { get; set; }
-        public int imagecount { get; set; }
-        public List<Image> images { get; set; }
+        public Matrix<double> asteroid;
     }
 }

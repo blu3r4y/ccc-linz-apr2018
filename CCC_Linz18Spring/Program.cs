71ae80e4374e3e72b0a83c9001ad4e61d6080be8
Mario Kahlhofer <mario.kahlhofer@gmail.com>
2018-11-17 14:02:10 +0100

    Remove unused references

--- CCC_Linz18Spring/Program.cs
+++ CCC_Linz18Spring/Program.cs
@@ -1,14 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Globalization;
 using System.IO;
 using System.Linq;
-using System.Threading;
-using MathNet.Numerics.LinearAlgebra;
-using MathNet.Spatial.Euclidean;
-using MoreLinq;
 using Newtonsoft.Json;
-using Whiteparse;
 
 namespace CCC_Linz18Spring
 {
